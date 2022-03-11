87449dc39860d7604f8f45f9b4667cc1410a3c40
YounggikimA <rladudrl286@naver.com>
2022-03-11 14:58:15 +0900

    basic REST API done.

--- src/main/java/com/study/todo/persistence/TodoRepository.java
+++ src/main/java/com/study/todo/persistence/TodoRepository.java
@@ -2,8 +2,13 @@ package com.study.todo.persistence;
 
 import com.study.todo.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    @Query("select * from Todo t where t.userId = ?1")
+    List<TodoEntity> findByUserId( String userId);
 }
